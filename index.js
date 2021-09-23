1ce22523e167cd4f5cc340f967d0e6dd3bec5cb5
MaxusGuitar <maxim1999chikhanov@gmail.com>
2021-09-23 22:19:16 +0300

    111111

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
+const refs = {
+    startBtn: document.querySelector('[data-action="start"'),
+    stopBtn: document.querySelector('[data-action="stop"'),
+  },
+  colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ],
+  randomIntegerFromInterval = (max) => {
+    return Math.floor(Math.random() * (max + 1));
+  },
+  setRandomColor = () => {
+    const color = colors[randomIntegerFromInterval(colors.length - 1)]
+    console.log(`RUN! ${color}`)
+    document.body.style.backgroundColor = color
+  }
 
-const startBtn = document.querySelector("[data-action='start']");
-const stopBtn = document.querySelector("[data-action='stop']");
-let timerId = null;
+let timerId = undefined
 
-startBtn.addEventListener("click", () => {
-  timerId = setInterval(() => {
-    console.log(randomIntegerFromInterval);
-  }, 1000);
-});
+refs.startBtn.addEventListener('click', () => {
+  timerId = timerId ?
+    timerId :
+    setInterval(() => setRandomColor(), 1000)
+})
 
-stopBtn.addEventListener("click", () => {
-  clearInterval(timerId);
-  console.log("setInterval stopped!");
-});
-
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
+refs.stopBtn.addEventListener("click", () => {
+ console.log(`СТОПЕ!!!! ${clearInterval(timerId)}`) 
+})
